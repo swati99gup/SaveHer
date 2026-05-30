ea4e9da94866a9095687e971d575f9d54f790337
swati99gup <swatigupta060605@gmail.com>
2026-05-30 22:51:43 +0530

    move firebase config to env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -7,13 +7,13 @@ import {
 from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZimPa0ETsBnsGoCcAf9UDelPx2ujgToE",
-  authDomain: "safeher-ddaad.firebaseapp.com",
-  projectId: "safeher-ddaad",
-  storageBucket: "safeher-ddaad.firebasestorage.app",
-  messagingSenderId: "325144050834",
-  appId: "1:325144050834:web:b196b848d344d284f3ba4b",
-  measurementId: "G-7YN00Y4ZS9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 const app =
   initializeApp(firebaseConfig);
