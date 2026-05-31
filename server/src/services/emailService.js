836d91bb23b67768a28cef3cbf555d19e34b5ad3
swati99gup <swatigupta060605@gmail.com>
2026-05-31 10:09:05 +0530

    smtp 587

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -7,14 +7,18 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 const transporter =
   nodemailer.createTransport({
 
-    service: "gmail",
-
-    auth: {
+ host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+      auth: {
 
       user:process.env.EMAIL_USER,
 
       pass:process.env.EMAIL_PASS,
-    }
+    },
+     tls: {
+    rejectUnauthorized: false,
+  },
   });
   
 
