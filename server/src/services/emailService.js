f3631e998182fa2e37f7adeab668f911f6647292
swati99gup <swatigupta060605@gmail.com>
2026-05-31 10:42:37 +0530

    Update email transporter to use secure connection

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -7,22 +7,16 @@ console.log(
   "EMAIL_PASS LENGTH:",
   process.env.EMAIL_PASS?.length
 );
-const transporter =
-  nodemailer.createTransport({
-
- host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-      auth: {
-
-      user:process.env.EMAIL_USER,
-
-      pass:process.env.EMAIL_PASS,
-    },
-     tls: {
-    rejectUnauthorized: false,
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
-  });
+});
   
 transporter.verify(function (error, success) {
   if (error) {
