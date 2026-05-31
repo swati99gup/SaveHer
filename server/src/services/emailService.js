2a646a0c7fa01b5967f00575df7448e84022d5cd
swati99gup <swatigupta060605@gmail.com>
2026-05-31 10:20:45 +0530

    verify smtp

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -3,7 +3,10 @@ dotenv.config();
 import nodemailer
 from "nodemailer";
 console.log("EMAIL_USER:", process.env.EMAIL_USER);
-
+console.log(
+  "EMAIL_PASS LENGTH:",
+  process.env.EMAIL_PASS?.length
+);
 const transporter =
   nodemailer.createTransport({
 
@@ -21,7 +24,13 @@ const transporter =
   },
   });
   
-
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("VERIFY ERROR:", error);
+  } else {
+    console.log("SMTP READY");
+  }
+});
 export const sendSOSMail =
 async (
 
